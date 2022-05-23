052751f1c1538c8a7700759b5e7bab7d310fd238
Ankit Kumar <ankit.kumar@sprinklr.com>
2022-05-23 11:33:07 +0530

    add clicks

--- script.js
+++ script.js
@@ -23,3 +23,14 @@ nav_element[0].classList.add("bluebutton");
 let curr_nav=0;//for current navigation
 myImage.setAttribute("src",data[0].previewImage);
 myTitle.value=data[0].title;
+//adding click functionality on navigation bar
+for(let i=0;i<nav_element.length;i++)
+{
+    nav_element[i].addEventListener("click",()=>{
+        nav_element[curr_nav].classList.remove("bluebutton");
+        nav_element[i].classList.add("bluebutton");
+        myTitle.value=data[i].title;
+        myImage.setAttribute("src",data[i].previewImage);
+        curr_nav=i;
+    })
+}
